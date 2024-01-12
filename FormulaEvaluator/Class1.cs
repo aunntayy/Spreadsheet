ab4453a6a978ef50ed172db8960193c2041c5364
phucm <phucm@LAPTOP-3M5U5422>
2024-01-12 14:47:07 -0700

    added information

--- FormulaEvaluator/Class1.cs
+++ FormulaEvaluator/Class1.cs
@@ -2,8 +2,28 @@
 
 namespace FormulaEvaluator
 {
-    public class haha
+    /// <summary>
+    /// Author:    Phuc Hoang
+    /// Partner:   -None-
+    /// Date:      1-Jan-2024
+    /// Course:    CS 3500, University of Utah, School of Computing
+    /// Copyright: CS 3500 and Phuc Hoang - This work may not 
+    ///            be copied for use in Academic Coursework.
+    ///
+    /// I, Phuc Hoang, certify that I wrote this code from scratch and
+    /// did not copy it in part or whole from another source.  All 
+    /// references used in the completion of the assignments are cited 
+    /// in my README file.
+    ///
+    /// File Contents
+    ///
+    ///    [This library evaluates arithmetic expressions using standard infix notation]
+    ///    
+    /// </summary>
+
+
+    public class Evaluator
     {
-        ReturnTypeEncoder ;
+       
     }
 }
