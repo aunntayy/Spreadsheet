43c7b845066068d9affef041af3ed76f128f867f
phucm <phucm@LAPTOP-3M5U5422>
2024-01-16 13:12:14 -0700

    added readme

--- FormulaEvaluatorTester/FormulaEvaluatorTester.cs
+++ FormulaEvaluatorTester/FormulaEvaluatorTester.cs
@@ -4,20 +4,25 @@ using FormulaEvaluator;
 
 try
 {
-    //Console.WriteLine((FormulaEvaluator.Evaluator.Evaluate("4 + 2", null)) == 6);
-    //Console.WriteLine((FormulaEvaluator.Evaluator.Evaluate("4-2", null)) == 2);
-    //Console.WriteLine((FormulaEvaluator.Evaluator.Evaluate("(2)", null)) == 2);
-    //Console.WriteLine((FormulaEvaluator.Evaluator.Evaluate("4/2", null)) == 2);
-    //Console.WriteLine((FormulaEvaluator.Evaluator.Evaluate("(3-1)*2", null)) == 4);
-    //Console.WriteLine((FormulaEvaluator.Evaluator.Evaluate("2*(3-1)", null)) == 4);
-    //Console.WriteLine((FormulaEvaluator.Evaluator.Evaluate("(3-1)/2", null)) == 1);
-    //Console.WriteLine((FormulaEvaluator.Evaluator.Evaluate("(6/2)*2", null)) == 6);
-    //Console.WriteLine((FormulaEvaluator.Evaluator.Evaluate("(6)", null)) == 6);
-    //Console.WriteLine((FormulaEvaluator.Evaluator.Evaluate("(2+3)/5", null)) == 1);
-    //Console.WriteLine((FormulaEvaluator.Evaluator.Evaluate("2+1*4", null)) == 6);
-    //Console.WriteLine((FormulaEvaluator.Evaluator.Evaluate("(2+1)*4", null)) == 12);
-    //Console.WriteLine((FormulaEvaluator.Evaluator.Evaluate("2/1*4", null)) == 8);
-    //Console.WriteLine((FormulaEvaluator.Evaluator.Evaluate("-2+4", null)) == 2);
+    //Test for basic math
+    Console.WriteLine((FormulaEvaluator.Evaluator.Evaluate("5 + 2", null)) == 7);
+    Console.WriteLine((FormulaEvaluator.Evaluator.Evaluate("5-2", null)) == 3);
+    Console.WriteLine((FormulaEvaluator.Evaluator.Evaluate("(2)", null)) == 2);
+    Console.WriteLine((FormulaEvaluator.Evaluator.Evaluate("4/2", null)) == 2);
+
+    //Test for mutilayer math
+    Console.WriteLine((FormulaEvaluator.Evaluator.Evaluate("(3-1)*2", null)) == 4);
+    Console.WriteLine((FormulaEvaluator.Evaluator.Evaluate("2*(3-1)", null)) == 4);
+    Console.WriteLine((FormulaEvaluator.Evaluator.Evaluate("(3-1)/2", null)) == 1);
+    Console.WriteLine((FormulaEvaluator.Evaluator.Evaluate("(6/2)*2", null)) == 6);
+    Console.WriteLine((FormulaEvaluator.Evaluator.Evaluate("(6)", null)) == 6);
+    Console.WriteLine((FormulaEvaluator.Evaluator.Evaluate("(2+3)/5", null)) == 1);
+    Console.WriteLine((FormulaEvaluator.Evaluator.Evaluate("2+1*4", null)) == 6);
+    Console.WriteLine((FormulaEvaluator.Evaluator.Evaluate("(2+1)*4", null)) == 12);
+    Console.WriteLine((FormulaEvaluator.Evaluator.Evaluate("2/1*4", null)) == 8);
+    Console.WriteLine((FormulaEvaluator.Evaluator.Evaluate("2 + 4", null)) == 6);
+    
+    // Test for varible with assigned value 
     // Define a Lookup delegate to provide values for variables
     FormulaEvaluator.Evaluator.Lookup variableLookup = (variableValue) =>
     {
@@ -32,11 +37,7 @@ try
     Console.WriteLine((FormulaEvaluator.Evaluator.Evaluate("x*y", variableLookup)) == 6);
     Console.WriteLine((FormulaEvaluator.Evaluator.Evaluate("x+y", variableLookup)) == 5);
     Console.WriteLine((FormulaEvaluator.Evaluator.Evaluate("x/y", variableLookup)) == 0);
-    Console.WriteLine((FormulaEvaluator.Evaluator.Evaluate("(x+A200)*2/5", variableLookup)) == 2);
-    // Test the expression with variables
-
-
-
+    //Console.WriteLine((FormulaEvaluator.Evaluator.Evaluate("(x+A200)*2/5", variableLookup)) == 2);
 }
 catch (Exception e)
 {
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# Spreadsheet
+```
+Author:     Phuc Hoang
+Partner:    None
+Start Date: 10-Jan-2023
+Course:     CS 3505, University of Utah, School of Computing
+GitHub ID:  PhucHoang123
+Repo:       https://github.com/Utah-School-of-Computing-de-St-Germain
+Commit Date: Day-Month-Year Time (of when submission is ready to be evaluated)
+Solution:   Spreadsheet
+Copyright:  CS 3500 and [Your Name(s)] - This work may not be copied for use in Academic Coursework.
+```
+
+# Overview of the Spreadsheet functionality
+
+The Spreadsheet program is currently capable of calculate basic math function with using infix notation.
+Future extensions will be added
+
+# Time Expenditures:
+
+    1. Assignment One:   Predicted Hours:          10        Actual Hours:   7
